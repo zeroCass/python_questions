fcd44c7b98fe5ff1e8748e79f5d11867b1a3528f
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-11-22 17:12:38 -0300

    fix: bullet_point fixed
    string eh interpretado como sendo um lista de caracteres. Entao o join trata cada carctere individualmente. O erro era devido a isso

---
+++ simple/book_questions/bullet_point_adder.py
@@ -0,0 +1,17 @@
+'''
+O script bulletPointAdder.py obterá o texto do clipboard, adicionará um
+asterisco e um espaço no início de cada linha e, em seguida, colará esse novo
+texto no clipboard.
+'''
+
+import sys, pyperclip
+
+text = pyperclip.paste()
+lines = text.split('\n')
+
+
+for idx in range(len(lines)):
+    lines[idx] = '* '  + lines[idx]
+
+text = '\n'.join(lines)
+pyperclip.copy(text)
---
+++ simple/book_questions/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    text = lists[idx]
