eb6637b9492765c383b1b27200b0c6ec9c6ac1a5
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-11-23 17:10:13 -0300

    regex part 1

--- simple/book_questions/regex.py
+++ simple/book_questions/regex.py
@@ -11,7 +11,7 @@ mo = phone_number_regex.search('My number phone is 415-555-4242')
 
 # obj match tem um metodo group que ira retornar a string com o texto pesquisado caso encontre
 print(f'Phone number found: {mo.group()}')
-
+print()
 
 ####################################
 # adicionar paratenses separa o obj regex em grupos
@@ -20,4 +20,27 @@ phone_number_regex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phone_number_regex.search('My number phone is 415-555-4242')
 print(f'Group 1: {mo.group(1)}')
 print(f'Group 2: {mo.group(2)}')
-print(f'All Groups: {mo.group()}') 
+print(f'All Groups: {mo.group()}')
+print()
+
+
+
+# adicionar caractres scapados por \
+phone_number_regex = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
+mo = phone_number_regex.search('My number phone is (415)-555-4242')
+print(f'Number with (): {mo.group()}')
+print()
+
+
+
+# correspondencia de varios grupos com pipe (|)
+# eh possivel fazer uma regex conter um grupo na qual ira retornar o primeiro grupo achado quando pesquisado
+
+# grupo regex com dois grupos separados por |
+heroRegex = re.compile (r'Batman|Tina Fey') 
+mo1 = heroRegex.search('Batman and Tina Fey.')
+print(mo1.group())
+
+mo1 = heroRegex.search('Tina Fey and Batman.')
+print(mo1.group())
+print()
