11dd306ca57306d338a4d723f71ceb32a1c97bfd
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-11-24 17:08:33 -0300

    feat: ragex test

--- book_questions/regex_test.py
+++ book_questions/regex_test.py
@@ -1,9 +1,20 @@
 import sys, os
 
+
+# get the dir path of current pathname module
+dir_base = os.path.dirname(os.path.abspath(__file__))
+
+# retorna o path da concatenacao do path + obj path
+file_dir = os.path.join(dir_base, 'files')
+
+# pega o file
 file_name = 'wiki.txt'
+file = os.path.join(dir_base, file_dir, file_name)
+
+
 try:
-    with open(file_name, 'r'):
-        file = file_name.read()
-    
+    with open(file) as file:
+        print(file.read())
 except FileNotFoundError as e:
     print(e)
+
