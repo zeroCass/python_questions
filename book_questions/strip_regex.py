c230a635b2d6f8889c4cdbbc892754b66a228941
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-11-26 17:31:38 -0300

    next-feat: strip version using regex

---
+++ book_questions/strip_regex.py
@@ -0,0 +1,13 @@
+'''
+Livro - Automatize Tarefas Macantes com Python by Al Sweigart
+Capitulo 7
+
+Projetos práticos:
+
+Versão de strip() usando regex
+Crie uma função que receba uma string e faça o mesmo que o método de
+string strip(). Se nenhum outro argumento for passado além da string em que
+a remoção será feita, os caracteres de espaço em branco serão removidos do
+início e do fim da string. Caso contrário, os caracteres especificados no
+segundo argumento da função serão removidos da string.
+'''
