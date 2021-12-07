4af9151108837543593f4f18ca03fefdb0f18005
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-12-07 17:32:57 -0300

    chapter 9 removing files

---
+++ book_questions/chapter_9/removing_files.py
@@ -0,0 +1,18 @@
+import os, shutil ,send2trash
+
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+# unlink deleta permanentemente um arquivo
+file2delete = os.path.join(dir, 'mad_libs.txt')
+if os.path.exists(file2delete):
+    os.unlink(file2delete)
+    print(f'O arquivo {file2delete} foi excluido')
+
+# os.rmdir -> remove uma pasta vazia
+
+# shutil.rmtree -> remove a pasta e todos os arquivos dentro
+dir2delete = os.path.join(dir, 'provas_backup')
+if os.path.exists(dir2delete):
+    shutil.rmtree(dir2delete)
+    print(f'A pasta {dir2delete} foi deleteada')
