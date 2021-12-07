627a4b8421c3b232fd68b39466b3394b3489027f
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-12-07 17:14:28 -0300

    feat: chapter 9 move/copy files

---
+++ book_questions/chapter_9/shutil_py.py
@@ -0,0 +1,33 @@
+
+
+import shutil, os
+
+# change diretory
+os.chdir('c:\\')
+# get current working directory
+print(os.getcwd())
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print(os.getcwd())
+
+# go to the previos path and get in chapter_8 folder
+chapter_8 = (os.path.join(os.path.abspath('..'), 'chapter_8'))
+
+
+# shitil.copy -> copia UM arquivo dado um path para outro path
+file_name = 'mad_libs.txt'
+shutil.copy(os.path.join(chapter_8, file_name), os.getcwd())
+# obs: se o arquiivo existir, ira sobrescreber
+
+# copytree -> copia todos os arquivos e subpastas dentro de um diretorio para outra pasta (cria essa pasta)
+file_name = 'provas_files'
+try:
+    shutil.copytree(os.path.join(chapter_8, file_name), os.path.join(os.getcwd(), 'provas_backup'))
+except FileExistsError:
+    print('Arquivo já existe')
+
+
+# move -> move/renomeia um arquivo de um lugar para outro
+file_name = 'mad_libs.txt'
+if not os.path.exists(os.path.join(os.getcwd(), 'mad_copy.txt')):
+    shutil.move(os.path.join(chapter_8, file_name), os.path.join(os.getcwd(), 'mad_copy.txt'))
