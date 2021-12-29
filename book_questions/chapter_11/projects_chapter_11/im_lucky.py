7177e1653adc36fc8e1f44d9722410caa2d91548
Mateus Valério Fernandes <mateusvalerio1@gmail.com>
2021-12-29 16:38:19 -0300

    feat: google scrape funciona mas n sei pq

--- book_questions/chapter_11/projects_chapter_11/im_lucky.py
+++ book_questions/chapter_11/projects_chapter_11/im_lucky.py
@@ -1,18 +1,30 @@
-import bs4, webbrowser, requests
-import sys, os
+import bs4
+import webbrowser
+import requests
+import sys
+import os
+import time
 
 
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 
 def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    #assert len(sys.argv) > 1, 'Error! The command cannot be empty'
-    
-    res = requests.get('https://www.google.com/search?q='+  ''.join(sys.argv[1:]), headers=headers)
+    assert len(sys.argv) > 1, 'Error! The command cannot be empty'
+
+    headers = {
+        'cookie': 'CGIC=InZ0ZXh0L2h0bWwsYXBwbGljYXRpb24veGh0bWwreG1sLGFwcGxpY2F0aW9uL3htbDtxPTAuOSxpbWFnZS93ZWJwLGltYWdlL2FwbmcsKi8qO3E9MC44LGFwcGxpY2F0aW9uL3NpZ25lZC1leGNoYW5nZTt2PWIz; HSID=AenmNVZxnoADsXz_x; SSID=AjbLhhwkjh8f3FOM8; APISID=IqkNtUA0V2DXlees/A0tA9iPSadMC2X6dt; SAPISID=8-N4B06I_D5N1mvR/AleccT6Zt0QllrukC; CONSENT=YES+UA.en+; OTZ=5204669_48_48_123900_44_436380; SID=rAd3UAFN_dCIGQ87HqDZZGiNyxdz0dL4dZKy_XquqSr_CHTzqSzfDdNTfLmA2xCMEZOZMA.; ANID=AHWqTUnDWUSHdvWhJiIoPxMAKYXmVtHCQIq7LBMYgiSlZZr3AMGTwY2aVUdjeY7z; NID=193=QImFbOa1vnKpflG8yJytqPXbJYJ9k8fWbIzQMGExsMa4g5oJwdnI56WNjgEVFAyAPJ1SEEOQ-zlW4HAUv-JLj0yAUImTgeT1syDIgFTMWAqxdz10lWRlzFC-3Fmjv6xJcqm2o6RKI50dmb7GetiheNdSAYPkAjng_c0lOHoXZLmtMwFOpkPTrQwVyUW8R2x4o1ux3OW3_kEbR_BREowRV8lVqrsnyo1ffC_Pm40zf81k7aS0cv9esYweGHF6Lxd532z4wA; 1P_JAR=2019-12-06-16; DV=k7BRh0-RaJtZsO9g7sjbrkcKoUjC7RYhxDh5AdfYgQAAAID1UoVsAVkvPgAAAFiry7niUB6qLgAAAGCQehpdCXeKnikKAA; SEARCH_SAMESITE=CgQIvI4B; SIDCC=AN0-TYv-lU3aPGmYLEYXlIiyKMnN1ONMCY6B0h_-owB-csTWTLX4_z2srpvyojjwlrwIi1nLdU4',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/75.0.3770.142 Chrome/75.0.3770.142 Safari/537.36'
+    }
+
+    res = requests.get('https://www.google.com/search', headers=headers, params={'q': ''.join(sys.argv[1:])})
     #res = requests.get('https://www.youtube.com/results?search_query=' + '+'.join(sys.argv[1:]))
     res.raise_for_status()
+    # if res.status_code == 429:
+    #     time.sleep(int(res.headers["Retry-After"]))
 
+    for link in bs4.BeautifulSoup(res.text, 'html.parser').select('a'):
+        print(link.get('href'))
 
 if __name__ == '__main__':
     main()
+
