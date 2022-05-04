bb8dba508fba2539cb0af35b46a915b6d4fb879e
Mateus Valério Fernandes <05694223101@saude.df.gov.br>
2022-05-04 16:02:42 -0300

    feat(weather_api): implementation completed

--- book_questions/chapter_14/projects/weather_request.py
+++ book_questions/chapter_14/projects/weather_request.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import sys
-from datetime import date
+from datetime import date, datetime, timedelta
 
 wheater_site_url = 'http://api.openweathermap.org/data/2.5/forecast?'
 params = {
@@ -9,7 +9,7 @@ params = {
     # 'lat': '',
     # 'lon': '',
     'appid': '5480412bb20e1c99fd10d525a1633add',
-    'cnt': '',
+    'cnt': '1',
     'mode': 'json',
     'lang': 'pt_br',
     'units': 'metric' #for celsius 
@@ -17,7 +17,26 @@ params = {
 
 
 def parse_json(json_file) -> str:
+    ''' simple convert the json file into python data '''
     return json.loads(json_file)
+
+
+
+
+def print_dict(data: list) -> None:
+    ''' will print formated data from dict to user '''
+
+    d = date.today()
+    for x in data:
+        print(f'''
+            Date: {d.strftime('%d/%m/%Y')}
+            Temperature: {x['temp']}
+            Min: {x['temp_min']}, Max: {x['temp_max']}
+            Feels like: {x['feels_like']}%
+            Humidity: {x['humidity']}
+            Description: {str(x['description']).capitalize()}\n''')
+        d += timedelta(days=1)
+
 
 
 def request_weather(params: dict) -> None:
@@ -26,41 +45,49 @@ def request_weather(params: dict) -> None:
         this function will use request module to get page content
         and it will be convert the data using another function parse_json
     '''
-    res = requests.get(wheater_site_url, params=params)
-    res.raise_for_status()
+    try:
+        res = requests.get(wheater_site_url, params=params)
 
-    # data = obj json where contais a list of each day
-    data = parse_json(res.text)
-    data = data['list']
-    main_info = data[0]['main']
-    weather = data[0]['weather']
+        # data = obj json where contais a list of each day
+        data = parse_json(res.text)
+        data = data['list']
 
-    #print(main_info)
-    #print(weather)
+        info = {
+            'temp': '',
+            'feels_like': '',
+            'temp_min': '',
+            'temp_max': '',
+            'humidity': '', 
+            'description': '',
+        }
 
-    today = date.today()
+        array_of_info = []
 
-    info = {
-        'temp': '',
-        'feels_like': '',
-        'temp_min': '',
-        'temp_max': '',
-        'humidity': '', 
-        'description': '',
-    }
+        # passing throght data extract from json
+        for idx in data:
+            new_info = {}
 
+            # idx['main'] = dictionay 
+            for key, value in idx['main'].items():
+                # if the key in the dictonary is the same as the info, insert the value
+                if key in info:
+                    new_info[key] = value
 
+            # weather info it is inside a dict named as 'wather', wich contains a single array
+            # inside this array contais a dict, wich has the info that we are looking for        
+            weather_dict = idx['weather'][0]
+            for key, value in weather_dict.items():
+                if key in info:
+                    new_info[key] = value
 
-    for idx in data:
+            array_of_info.append(new_info)
 
-        for key, value in idx['main'].items():
-            if key in info:
-                info[key] = value
-            
-        weather_list = idx['weather']
+        print_dict(array_of_info)
 
-
-    print(info)
+    except:
+        print('City not found. Try a valid one.')
+        sys.exit(1)
+        
 
         
 
@@ -69,11 +96,12 @@ def request_weather(params: dict) -> None:
 
 def main():
     if len(sys.argv) < 2:
-        print('Entrada invalida.\nModo de uso: python weather_request.py cidade (opcional)quantidade_de_dias(max:5)')
+        print('Entrada invalida.\nModo de uso: python weather_request.py city (optional)number_of_days(max:5)')
         sys.exit(1)
     
     params['q'] = sys.argv[1]
-    params['cnt'] = sys.argv[2]
+    if len(sys.argv) > 2: 
+        params['cnt'] = sys.argv[2]
     request_weather(params)
 
 
