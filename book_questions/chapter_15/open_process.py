fff708404edb827e2e2b6d9200aa6b79d8baa977
Mateus Valério Fernandes <05694223101@saude.df.gov.br>
2022-05-10 17:28:19 -0300

    error(prpcess): nao consigo achar uma logica que atenda a satisfacao

--- book_questions/chapter_15/open_process.py
+++ book_questions/chapter_15/open_process.py
@@ -1,8 +1,38 @@
-from re import sub
+import os
 import subprocess
-#lab_process = subprocess.Popen('C:\TrakCare\Lab2014\LabLIVE.bat')
-#calc = subprocess.Popen(['c:\\Windows\\System32\\calc.exe'])
-calc = subprocess.call('c:\\Windows\\System32\\calc.exe')
-music = subprocess.Popen('')
+import time
+import psutil
 
-print('Done')
+
+lab_process = subprocess.Popen('C:\TrakCare\Lab2014\LabLIVE.bat')
+lab_process.wait()
+
+#calc = subprocess.run(['C:\TrakCare\Lab2014\LabLIVE.bat'], capture_output=True, shell=True, text=True)
+# print(lab_process.pid)
+
+# output = os.popen('wmic process get processid').read()
+# print(output)
+
+def check_process(processid):
+    all_process = os.popen('wmic process get processid').read()
+    #print(f'The process: {all_process}')
+    if str(processid) in all_process:
+        print('Found it')
+        return True
+    print('Dont found it')
+    return False
+
+
+alive = check_process(lab_process.pid)
+print(f'LABID: {lab_process.pid}')
+
+last_t = time.perf_counter()
+while alive:
+    t = time.perf_counter()
+    if (t - last_t)*1000 > 1000:
+        print('entrou')
+        alive = check_process(lab_process.pid)
+        last_t = time.perf_counter()
+    
+
+print('Done\nProcess killed')
