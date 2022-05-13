105a2196fa6c64abf007673c2392eccbaac104e6
Mateus Valério Fernandes <05694223101@saude.df.gov.br>
2022-05-13 17:11:53 -0300

    feat(email_sender): smtp conection and email sender works

--- book_questions/chapter_15/open_process.py
+++ book_questions/chapter_15/open_process.py
@@ -5,54 +5,59 @@ import sys
 import psutil
 import json
 import wmi
+import datetime
 
 
-all_process = []
-# for p in psutil.process_iter():
-#     #pinfo = p.as_dict(attrs=['pid', 'name', 'create_time'])
-#     all_process.append(p.pid)
+process = subprocess.Popen('C:\Windows\System32\calc', creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP, shell=False)
+#process = subprocess.Popen('C:\Windows\System32\calc', creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#os.startfile('C:\Windows\System32\calc')
 
-#all_process = os.popen('wmic process get processid').read()
+process_name = 'calculator.exe'
+# esse pid deixara de existir quando process.poll() == 0
+print(process.pid)
+process.wait()
+
+running = True
+while running:
+    running = process.pid in (p.pid for p in psutil.process_iter())
+    print('Running')
+print('Done')
+
+# loop para verifica os processos criados recentemente
+
+for p in psutil.process_iter():
+    create_h = datetime.datetime.fromtimestamp(p.create_time())
+    t = datetime.datetime.now() - datetime.timedelta(minutes=5)
+    # verifica se o tempo de criacao do processo eh maior do 5min atras
+    if create_h > t:
+        # mostra os processos recentes
+        print(p)
+
+# gera a lista de processos filhos
+current_p = psutil.Process()
+children = current_p.children(recursive=True)
+print(children) # lista retorna vazia
+for child in children:
+    print(f'Child pid: {child.pid}')
+
+
+print('Done')
+
+# ESTE FUNCIONA
+# c = wmi.WMI()
+# process_watcher = c.Win32_Process.watch_for("creation")
+# lab_process = subprocess.Popen('C:\TrakCare\Lab2014\LabLIVE.bat')
+# while True:
+#     new_process = process_watcher()
+#     print(new_process.Caption,new_process.ProcessId)
 
 
 
-c = wmi.WMI()
-process_watcher = c.Win32_Process.watch_for("creation")
-lab_process = subprocess.Popen('C:\TrakCare\Lab2014\LabLIVE.bat')
-while True:
-    new_process = process_watcher()
-    print(new_process.Caption,new_process.ProcessId)
-
-# with open('otuput.txt', 'w') as f:
-#     for line in all_process:
-#         for key, value in line.items():
-#             f.write(f'{key}:{value}\t')
-#         f.write('\n')
-
-
-
-sys.exit(0)
 
 #calc = subprocess.run(['C:\TrakCare\Lab2014\LabLIVE.bat'], capture_output=True, shell=True, text=True)
 # print(lab_process.pid)
 
 
-def check_process(processid):
-    process = os.popen('wmic process get processid').read()
-    #print(f'The process: {all_process}')
-    if str(processid) in process:
-        print('Found it')
-        return True
-    print('Dont found it')
-    return False
 
 
-
-last_t = time.perf_counter()
-alive = check_process(processid)
-while alive:
-    t = time.perf_counter()
-    if (t - last_t)*1000 > 1000:
-        alive = check_process(processid)
-        last_t = time.perf_counter()
     
---
+++ book_questions/chapter_16/userinput.py
@@ -0,0 +1,2 @@
+x = 10
+for i in range(x):
