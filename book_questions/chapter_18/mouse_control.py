19664de1872e93d026ab2dddfbd9b2aa9997f225
Mateus Valério Fernandes <05694223101@saude.df.gov.br>
2022-05-20 17:39:47 -0300

    feat(chp_18): mouse autogui init

---
+++ book_questions/chapter_18/mouse_control.py
@@ -0,0 +1,12 @@
+import pyautogui
+pyautogui.PAUSE = 1         # this pause the automation to the user assume the control
+pyautogui.FAILSAFE = True   # if the mouse goes out the screen, the program will fail
+
+
+pyautogui.click()
+pyautogui.
+pyautogui.moveTo(100, 100, duration=0.25)
+pyautogui.moveTo(200, 100, duration=0.25)
+pyautogui.moveTo(200, 200, duration=0.25)
+pyautogui.moveTo(100, 200, duration=0.25)
+pyautogui.moveTo(100, 100, duration=0.25)
